9942f4d47a368d8863d87035fa820bfd8f065269
いちごミルク <119553147+ichigomilk15@users.noreply.github.com>
2025-06-06 14:26:30 +0900

    Add triangle rendering

--- DX11Wrapper.h
+++ DX11Wrapper.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <d3d11.h>
+#include <d3dcompiler.h>
 #include <wrl/client.h>
+
+struct SimpleVertex
+{
+    float x, y, z;
+};
 
 class DX11Wrapper
 {
@@ -12,10 +18,17 @@ public:
     bool Initialize(HWND hwnd, int width, int height);
     void Clear(float r, float g, float b, float a);
     void Present();
+    void Draw();
 
 private:
     Microsoft::WRL::ComPtr<ID3D11Device> device_;
     Microsoft::WRL::ComPtr<ID3D11DeviceContext> context_;
     Microsoft::WRL::ComPtr<IDXGISwapChain> swapChain_;
     Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView_;
+    Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer_;
+    Microsoft::WRL::ComPtr<ID3D11VertexShader> vertexShader_;
+    Microsoft::WRL::ComPtr<ID3D11PixelShader> pixelShader_;
+    Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout_;
+
+    bool CreateTriangle();
 };
